8efd3648ea5fb1336abcfd8761de1574494af54c
pepoppe <72536699+Sinkung123@users.noreply.github.com>
2021-12-08 08:56:50 +0700

    Add files via upload

---
+++ Product v.1.2.1/cart.js
@@ -0,0 +1,45 @@
+
+export let cart = [];
+
+const countProduct = document.querySelector('#count');
+
+const deleteBtn = document.querySelector('#delete');
+deleteBtn.addEventListener("click",removeCart);
+
+
+export function addCart(event)
+{ 
+    let id = event.target.id;
+    let qty = 1 ;
+    let x = cart.filter( cart => cart.pId == id );
+    if(cart.find( id => { return id === x[0];})!= undefined)
+        {
+            cart[cart.findIndex( id => { return id === x[0];})].qty += 1;
+            countProduct.textContent = countCart();
+            //console.log(cart);
+        }
+    else{
+        cart.push({pId : id,qty : qty} );
+        alert(` " ${id} " added in your cart`);
+        countProduct.textContent = countCart();
+        //console.log(cart);
+    }
+}
+
+export function removeCart()
+{ let x =[];
+    cart = x;
+    //console.log(cart);
+    countProduct.textContent = cart.length;
+}
+
+export function countCart(){
+    
+    let qt = 0;
+    for(let i=0;i<cart.length;i++){
+
+       qt += cart[i].qty;
+
+    }
+    return qt ;
+}
